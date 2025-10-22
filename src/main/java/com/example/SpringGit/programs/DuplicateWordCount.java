fb5fe988573f4cf4790b58b50108074978efd618
amberksingh <amberksingh95@gmail.com>
2025-10-22 19:36:07 +0530

    2nd merge conflict trial

--- src/main/java/com/example/SpringGit/programs/DuplicateWordCount.java
+++ src/main/java/com/example/SpringGit/programs/DuplicateWordCount.java
@@ -12,7 +12,7 @@ public class DuplicateWordCount {
 
     public static void main(String[] args) {
 
-        String str = "hellO great morning hello world night great people people GREAT NIGHT EAGLE world eat";
+        String str = "hellO great wheat hello wheat night great people wheat GREAT NIGHT EAGLE feature world EAT";
         String[] words = str.toLowerCase().split(" ");
         
         //using Collectors.groupingBy
